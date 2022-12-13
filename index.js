6cb13e808860911cbdbd607529369ee4d2ffa30b
Jules <118351366+JulesNIYOMWUNGERI@users.noreply.github.com>
2022-12-13 13:08:56 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ function activeMenu () {
 } 
 activeMenu();
 window.addEventListener("scroll", activeMenu);
+
+
+const lists = document.querySelectorAll('.nlists');
+
+
+lists.forEach((list) => {
+    list.addEventListener('click', function(){
+        lists.forEach(li => li.classList.remove('now'));
+
+        this.classList.add('now')
+    })
+})
+
+
+const nav = document.querySelector('nav');
+
+function handleNav () {
+    nav.classList.toggle('sticky', window.scrollY > 200)
+}
+
+window.addEventListener('scroll',handleNav)
+
+
+
