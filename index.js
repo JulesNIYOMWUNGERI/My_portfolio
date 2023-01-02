dd8071747e9ed00285adc0b4dd8ae8d26d616cf8
Jules <118351366+JulesNIYOMWUNGERI@users.noreply.github.com>
2023-01-02 16:57:15 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -13,9 +13,22 @@ function activeMenu () {
 activeMenu();
 window.addEventListener("scroll", activeMenu);
 
+//SKILLS AND EXPRIENCE
 
 const lists = document.querySelectorAll('.nlists');
+const descs = document.querySelectorAll('.desc')
 
+function handletab(tabname) {
+    const def = document.querySelector('.default');
+    def.classList.add('now')
+
+    descs.forEach((desc)=>{
+        desc.classList.remove('active-tab')
+
+        const activeOne = document.getElementById(tabname)
+        activeOne.classList.add('active-tab')
+    })
+}
 
 lists.forEach((list) => {
     list.addEventListener('click', function(){
@@ -110,31 +123,39 @@ function validation(e){
 const hamburgerBtn = document.querySelector('.hamburger');
 
 const navlist = document.querySelector('.list')
+const bars = document.getElementById('bars')
+const closed = document.getElementById('close')
 
 
 function toggleBtn () {
-    navlist.classList.toggle('show')
+    navlist.classList.add('show')
 }
 
 hamburgerBtn.addEventListener('click', toggleBtn)
+
+
+function handleHide () {
+    navlist.classList.remove('show')
+}
 
 
 //LOCALSTORAGE
 
 function getBlog () {
     var blogs = JSON.parse(localStorage.getItem("blog"))
-    console.log(blogs)
 
-    document.getElementById('blogbody').innerHTML = blogs.map((blog) => `
-      <div class="smallblog1">
-        <img src="images/agency2.jpeg">
-        <div class="description">
-            <p>${blog?.title}</p>
-            <h1>${blog?.desc}</h1>
-            <button>Read More</button>
-        </div>
-      </div>`
-    )
+    for(var i = 0; i < blogs.length; i++){
+        document.getElementById('blogbody').innerHTML += `
+          <div class="smallblog1">
+            <img src=${blogs[i]?.img}>
+            <div class="description">
+              <p>${blogs[i]?.title}</p>
+              <h1>${blogs[i]?.desc}</h1>
+              <button>Read More</button>
+            </div>
+          </div>
+        `
+    }
 }
 
 
@@ -180,8 +201,66 @@ function handleSubmitBlog (e) {
         window.alert("Blog Added Successfully!.")
         getBlog();
     }
+}
 
+
+//SEND EMAIL
+
+function sendEmail () {
+    if(document.getElementById('username').value === ""){
+        document.getElementById('comfirm').classList.remove('comfirm')
+        document.getElementById('comfirm').classList.add('not_comfirm')
+        document.getElementById('comfirm').innerHTML='You must fill All the field first!'
+    }else if(document.getElementById('email').value === ""){
+        document.getElementById('comfirm').classList.remove('comfirm')
+        document.getElementById('comfirm').classList.add('not_comfirm')
+        document.getElementById('comfirm').innerHTML='You must fill All the field first!'
+    }else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(document.getElementById('email').value)){
+        document.getElementById('comfirm').classList.remove('comfirm')
+        document.getElementById('comfirm').classList.add('not_comfirm')
+        document.getElementById('comfirm').innerHTML='You must enter a valid email first!'
+    }else if(document.getElementById('subject').value === ""){
+        document.getElementById('comfirm').classList.remove('comfirm')
+        document.getElementById('comfirm').classList.add('not_comfirm')
+        document.getElementById('comfirm').innerHTML='You must fill All the field first!'
+    }else if(document.getElementById('message').value === ""){
+        document.getElementById('comfirm').classList.remove('comfirm')
+        document.getElementById('comfirm').classList.add('not_comfirm')
+        document.getElementById('comfirm').innerHTML='You must fill All the field first!'
+    }else{
+        document.getElementById('comfirm').innerHTML=''
+
+        var params = {
+            from_name : document.getElementById('username').value,
+            email_id : document.getElementById('email').value,
+            subject : document.getElementById('subject').value,
+            message : document.getElementById('message').value
+        }
+        
+        emailjs.send("service_v709atl", "template_4e1r3mc", params).then(function (res) {
+            document.getElementById('comfirm').classList.remove('not_comfirm')
+            document.getElementById('comfirm').classList.add('comfirm')
+            document.getElementById('comfirm').innerHTML='Message Sent Successfully!!'
     
-    
+            document.getElementById('form').reset()
+
+            document.getElementById('username').classList.remove('valid')
+            document.getElementById('username').classList.remove('invalid')
+
+            document.getElementById('email').classList.remove('valid')
+            document.getElementById('email').classList.remove('invalid')
+
+            document.getElementById('subject').classList.remove('valid')
+            document.getElementById('subject').classList.remove('invalid')
+
+            document.getElementById('message').classList.remove('valid')
+            document.getElementById('message').classList.remove('invalid')
+
+            setTimeout(function(){
+                document.getElementById('comfirm').innerHTML=''
+            },5000)
+
+        }).catch((err) => console.log(err))
+    }
 }
 
